0db657beee1a82bfb5a400f0af5cd20d2d5bdd27
cybon <kimon5@hotmail.dk>
2018-10-25 13:49:04 +0200

    asd

---
+++ WebAPILibrary/WebAPILibrary/SqlHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+
+namespace WebAPILibrary
+{
+    public class SqlHandler
+    {
+        private readonly string _connectionString;
+        private readonly string _sqlCommand;
+
+        private SqlConnection _conn;
+
+        public SqlDataReader Reader;
+
+        public SqlHandler(string cmd, string conn)
+        {
+            _sqlCommand = cmd;
+            _connectionString = conn;
+        }
+
+        public void SqlConnect()
+        {
+            using (_conn = new SqlConnection())
+            {
+                _conn.ConnectionString = _connectionString;
+                _conn.Open();
+            }
+        }
+
+
+
+
+
+    }
+}
