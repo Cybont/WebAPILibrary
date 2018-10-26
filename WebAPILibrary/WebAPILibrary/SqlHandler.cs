bedacb2a9fd4fab91a47ed37584310594ccdc980
cybon <kimon5@hotmail.dk>
2018-10-26 10:35:07 +0200

    kfkf

--- WebAPILibrary/WebAPILibrary/SqlHandler.cs
+++ WebAPILibrary/WebAPILibrary/SqlHandler.cs
@@ -6,10 +6,10 @@ using System.Data.SqlClient;
 
 namespace WebAPILibrary
 {
-    public class SqlHandler
+    public class SqlHandler<T, TKey>
     {
         private readonly string _connectionString;
-        private readonly string _sqlCommand;
+        //private readonly string _sqlCommand;
 
         private SqlConnection _conn;
 
@@ -21,41 +21,73 @@ namespace WebAPILibrary
         /// <param name="cmd">Querystring</param>
         /// <param name="conn">Connection string for Database</param>
         /// <param name="http">HTTP Actions: Get, GetOne Post, Put </param>
-        public SqlHandler(string cmd, string conn, string http = "")
+        public SqlHandler(string conn)
         {
-            _sqlCommand = cmd;
+            //_sqlCommand = cmd;
             _connectionString = conn;
 
-            SqlConnect();
+            //SqlConnect();
 
-            switch (http)
-            {
-                default:
-                    break;
-                case "Get":
-                    break;
-                case "GetOne":
-                    break;
-                case "Post":
-                    break;
-                case "Put":
-                    break;
-            }
+            //switch (http)
+            //{
+            //    default:
+            //        break;
+            //    case "Get":
+            //        break;
+            //    case "GetOne":
+            //        break;
+            //    case "Post":
+            //        break;
+            //    case "Put":
+            //        break;
+            //}
+
+            //_conn.Close();
         }
 
         public void SqlConnect()
         {
-            using (_conn = new SqlConnection())
-            {
                 _conn.ConnectionString = _connectionString;
                 _conn.Open();
+        }
+
+        public List<T> Get(string cmd)
+        {
+            SqlConnect();
+            SqlCommand command = new SqlCommand(cmd);
+            Reader = command.ExecuteReader();
+            List<T> list = new List<T>();
+            while (Reader.Read())
+            {
+
             }
+            
+            return list;
+        }
+        public void GetOne(string cmd, TKey key)
+        {
+            SqlConnect();
+
+            _conn.Close();
+        }
+        public void Post(string cmd, List<T> list)
+        {
+            SqlConnect();
+
+            _conn.Close();
+        }
+        public void Put(string cmd)
+        {
+            SqlConnect();
+
+            _conn.Close();
         }
 
 
-
-
-
+        public void Close()
+        {
+            _conn.Close();
+        }
 
 
     }
